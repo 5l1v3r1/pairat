fbb80c948d8c5aa19bfc3186503caa26d7dde57c
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-10-11 20:08:02 -0500

    works on windows?

--- src/utils/killProccess.go
+++ src/utils/killProccess.go
@@ -1,65 +1,75 @@
 package utils
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"os/signal"
+	"runtime"
 	"syscall"
 )
 
 func KillProcess() {
+	switch runtime.GOOS {
+	case "linux", "darwin":
+		/*
 
-	/*
+			node (client)
 
-		node (client)
+		*/
+		cmd := exec.Command("killall", "node")
+		go func() {
 
-	*/
-	cmd := exec.Command("killall", "node")
-	go func() {
-
-		if err := cmd.Run(); err != nil {
-			cmd = exec.Command("killall", "node")
 			if err := cmd.Run(); err != nil {
-				log.Println(err)
+				cmd = exec.Command("killall", "node")
+				if err := cmd.Run(); err != nil {
+					log.Println(err)
+				}
 			}
-		}
-	}()
+		}()
 
-	// stop the process when you type ctrl c
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-c
-		if err := cmd.Process.Kill(); err != nil {
-			log.Println(err.Error())
-		}
-		os.Exit(0)
-	}()
-	/*
+		// stop the process when you type ctrl c
+		c := make(chan os.Signal)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		go func() {
+			<-c
+			if err := cmd.Process.Kill(); err != nil {
+				log.Println(err.Error())
+			}
+			os.Exit(0)
+		}()
+		/*
 
-		Ngrok
+			Ngrok
 
-	*/
-	cmd2 := exec.Command("killall", "ngrok")
-	go func() {
+		*/
+		cmd2 := exec.Command("killall", "ngrok")
+		go func() {
 
-		if err := cmd2.Run(); err != nil {
-			cmd = exec.Command("killall", "ngrok")
 			if err := cmd2.Run(); err != nil {
-				log.Println(err)
+				cmd = exec.Command("killall", "ngrok")
+				if err := cmd2.Run(); err != nil {
+					log.Println(err)
+				}
 			}
-		}
-	}()
+		}()
 
-	// stop the process when you type ctrl c
-	a := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-a
-		if err := cmd.Process.Kill(); err != nil {
-			log.Println(err.Error())
-		}
+		// stop the process when you type ctrl c
+		a := make(chan os.Signal)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		go func() {
+			<-a
+			if err := cmd.Process.Kill(); err != nil {
+				log.Println(err.Error())
+			}
+			os.Exit(0)
+		}()
+	case "windows":
+		fmt.Println("make sure you have ngrok and node installed and that are not running , https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip, https://nodejs.org/es/download/")
+	default:
+		fmt.Println("No OS detected")
 		os.Exit(0)
-	}()
+	}
+
 }
